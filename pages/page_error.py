f8045c6314ef4b1a1aa9d207c4029fb6642af01a
keuhdall <keuhdall@gmail.com>
2026-04-03 11:15:28 +0200

    update error screen

--- pages/page_error.py
+++ pages/page_error.py
@@ -1,49 +1,46 @@
 from utils.minitel_ui import (
-    BLACK, RED, WHITE, BLUE, CYAN,
-    NORMAL_SIZE, DOUBLE_HEIGHT,
-    move, fg, bg, draw_hline, fill_row, clear_screen, write_centered,
+    BLACK, WHITE, BLUE,
+    NORMAL_SIZE, DOUBLE_SIZE,
+    move, fg, bg, fill_row, clear_screen, write_centered,
 )
 
 
 def draw(ser):
-    """Draw a WiFi connection error screen."""
+    """Draw a WiFi connection error screen (Windows BSOD style)."""
     clear_screen(ser)
 
-    # Title
-    fill_row(ser, 1, BLUE, WHITE)
-    fill_row(ser, 2, BLUE, WHITE)
-    move(ser, 2, 1)
+    # Fill entire screen with blue background
+    for row in range(1, 25):
+        fill_row(ser, row, BLUE, WHITE)
+
+    # Sad face :( in double size
+    move(ser, 4, 4)
     bg(ser, BLUE)
     fg(ser, WHITE)
-    ser.write(DOUBLE_HEIGHT)
-    ser.write("  3615 PARIS INFO")
+    ser.write(DOUBLE_SIZE)
+    ser.write(":(")
     ser.write(NORMAL_SIZE)
 
-    fill_row(ser, 3, BLUE, WHITE)
-
-    # Separator
-    draw_hline(ser, 5, 1, 40, 0x3F)
-
-    # Error message
-    move(ser, 9, 1)
-    fg(ser, RED)
-    write_centered(ser, 9, "ERREUR DE CONNEXION")
-
-    move(ser, 12, 1)
-    fg(ser, WHITE)
-    write_centered(ser, 12, "Impossible de se connecter")
-    write_centered(ser, 13, "au reseau WiFi.")
-
-    write_centered(ser, 16, "Verifiez votre connexion")
-    write_centered(ser, 17, "et redemarrez le serveur.")
-
-    # Separator
-    draw_hline(ser, 21, 1, 40, 0x3F)
-
-    # Footer
-    fill_row(ser, 24, BLUE, WHITE)
-    move(ser, 24, 1)
+    # Main error message
     bg(ser, BLUE)
     fg(ser, WHITE)
-    ser.write(" (C) 2025 PARIS INFO ")
-    bg(ser, BLACK)
+    move(ser, 9, 4)
+    ser.write("Votre Minitel a rencontre")
+    move(ser, 10, 4)
+    ser.write("un probleme et doit")
+    move(ser, 11, 4)
+    ser.write("redemarrer.")
+
+    # Details
+    move(ser, 14, 4)
+    ser.write("Connexion WiFi impossible.")
+
+    # Progress
+    move(ser, 17, 4)
+    ser.write("0% termine")
+
+    # Restart instruction
+    move(ser, 20, 4)
+    ser.write("Redemarrez le serveur pour")
+    move(ser, 21, 4)
+    ser.write("relancer la connexion.")
